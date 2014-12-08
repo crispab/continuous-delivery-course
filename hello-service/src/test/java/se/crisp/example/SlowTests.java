da2a0ffdb33070856a603309d933e14e9ef4c7b4
Magnus Axelqvist <magnus@axelqvist.com>
2014-12-08 23:31:08 +0100

    Move to new place

--- hello-service/src/test/java/se/crisp/example/SlowTests.java
+++
@@ -1,9 +0,0 @@
-package se.crisp.example;
-
-/**
- * Marker interface for slow tests (typically integration tests)
- *
- * @see org.junit.experimental.categories.Categories
- */
-public interface SlowTests {
-}
